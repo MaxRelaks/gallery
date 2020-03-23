9c06c899cc3ade78265589fd4054d07ec3466d0e
MaxRelaks <62361450+MaxRelaks@users.noreply.github.com>
2020-03-23 23:29:47 +0200

    sendmail 95%

--- models/User.php
+++ models/User.php
@@ -72,4 +72,39 @@ class User extends \yii\db\ActiveRecord
     {
         return $this->hasMany(Comment::className(), ['user_id' => 'id']);
     }
+    public function setPassword($password)
+    {
+        $this->password = sha1($password);
+    }
+
+    public function validatePassword($password)
+    {
+        return $this->password === sha1($password);
+    }
+
+    //=============================================
+    public static function findIdentity($id)
+    {
+        return self::findOne($id);
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+
+    }
+
+    public function getAuthKey()
+    {
+
+    }
+
+    public function validateAuthKey($authKey)
+    {
+
+    }
 }
