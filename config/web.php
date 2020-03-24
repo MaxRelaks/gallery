9591b4d013fe0e84d07b174ba382dc82a6343326
Max Morgushko <m_lenovo99@ukr.net>
2020-03-24 13:15:39 +0200

    update

--- config/web.php
+++ config/web.php
@@ -61,11 +61,6 @@ $config = [
             ],
         ],
     ],
-    'modules' => [
-        'admin' => [
-            'class' => 'app\modules\admin\Module',
-        ],
-    ],
     'params' => $params,
 ];
 
